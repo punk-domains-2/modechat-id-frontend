3a672ab7f4c7d3462fea67b0ea11106dcae0a735
Johnson Chen <chnejohnson@gmail.com>
2024-05-06 10:30:20 +0800

    upgrade vite and @vitejs/plugin-vue to fix Big Integer literals problem

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,4 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue()],
-	define: {
-		__VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false, // refer to https://stackoverflow.com/questions/77752897/feature-flag-vue-prod-hydration-mismatch-details-is-not-explicitly-defined
-	},
 })
